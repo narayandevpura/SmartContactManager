6aa73dd0d5ebdb3ddc8170449696712787e1e906
narayan.devpura <narayan.devpura@accenture.com>
2024-03-09 18:23:58 +0530

    aaded validations

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -4,9 +4,11 @@ import com.smart.dao.UserRepository;
 import com.smart.entities.User;
 import com.smart.helper.Message;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -38,13 +40,19 @@ public class HomeController {
     }
 
     @PostMapping("/do_register")
-    public String registerUser(@ModelAttribute("user") User user, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session) {
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session) {
 
         try {
             if (!agreement) {
                 throw new Exception("Please agree to the terms and conditions");
             }
 
+            if (bindingResult.hasErrors()) {
+                System.out.println("Error "+ bindingResult.toString());
+                model.addAttribute("user", user);
+                model.addAttribute("agreement", agreement);
+                return "signup";
+            }
             user.setRole("ROLE_USER");
             user.setEnabled(true);
             user.setImageUrl("default.png");
@@ -55,12 +63,13 @@ public class HomeController {
             User result = userRepository.save(user);
 
             model.addAttribute("user", new User());
-
+            model.addAttribute("agreement", agreement);
             session.setAttribute("message", new Message("Successfully Registered !!", "alert-success"));
             return "signup";
         } catch (Exception e) {
             e.printStackTrace();
             model.addAttribute("user", user);
+            model.addAttribute("agreement", agreement);
             session.setAttribute("message", new Message("Something went wrong !!"+e.getMessage(), "alert-danger"));
             return "signup";
         }
